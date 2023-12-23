01a52611ea32ecfeda4cbd148bf98947814c96f5
santiago-241432 <spdalessa@gmail.com>
2023-12-23 15:50:32 -0300

    actualizacion 4

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () =>{
     return(
-        <div className="text-center m-3">
+        <div className="text-center m-3" >
         <div className="spinner-border"  role="status">
             <span className="visually-hidden">Loading...</span>
         </div>
