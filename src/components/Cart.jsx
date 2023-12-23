126efc40fbbf9bc0f479a721481a8935d0b29405
santiago-241432 <spdalessa@gmail.com>
2023-12-23 14:37:28 -0300

    actualizacion 4

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -49,11 +49,9 @@ const Cart = () =>{
 
                         {
                             Cart.map(item=>(
-                            <tr className="bg-white border border-black" key={item.index}>
-                                <td><img src={item.imagen} alt={item.nombre} width={200} className="imagen-carrito"  /></td>
-                                <td>{item.nombre}</td>
-                                <td>{item.quantity} Gramos</td>
-                                <td>${item.precio * item.quantity }</td>
+                            <tr className="bg-white" key={item.index}>
+                                <td><img src={item.imagen} alt={item.nombre} width={200} className="imagen-carrito"  /><div>{item.nombre}</div></td>
+                                <td><div>{item.quantity} Gramos</div><div>${item.precio * item.quantity }</div></td>
                                 <td><Link onClick={() => {removeItem(item.index)}}><svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" class="bi bi-backspace-fill" viewBox="0 0 16 16">
                                 <path d="M15.683 3a2 2 0 0 0-2-2h-7.08a2 2 0 0 0-1.519.698L.241 7.35a1 1 0 0 0 0 1.302l4.843 5.65A2 2 0 0 0 6.603 15h7.08a2 2 0 0 0 2-2zM5.829 5.854a.5.5 0 1 1 .707-.708l2.147 2.147 2.146-2.147a.5.5 0 1 1 .707.708L9.39 8l2.146 2.146a.5.5 0 0 1-.707.708L8.683 8.707l-2.147 2.147a.5.5 0 0 1-.707-.708L7.976 8z"/>
                                 </svg></Link></td>
@@ -62,10 +60,10 @@ const Cart = () =>{
 
                             <tr className="border-0">
                                 <td></td>
-                                <td></td>
-                                <td></td>
                                 <td>${cartSumaTotal()}</td>
                                 <td><button className="btn m-3 btn-outline-dark"onClick={() =>{clear()}}>VACIAR CARRITO</button></td>
+                                <td></td>
+                                <td></td>
                             </tr>
                     </table>
                     
