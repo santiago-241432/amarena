50a810170cf4bfbc271f1dc96ac2320bd0d090f1
santiago-241432 <spdalessa@gmail.com>
2024-01-07 22:25:28 -0300

    actualizacion 17

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -7,10 +7,10 @@ const CartWidget = () =>{
     const {cartTotal} = useContext(CartContext);
 
     return(cartTotal()> 0)?
-        <Link to={"/cart"} type="button" className="btn btn-outline-dark position-absolute end-0 bg-black"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="white" class="bi bi-cart2 m-3" viewBox="0 0 16 16">
+        <Link to={"/cart"} type="button" width="16" height="16" className="btn btn-outline-dark position-absolute end-0"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-cart2 m-3" viewBox="0 0 16 16">
         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
         </svg>
-            <span className="position-absolute top-0 start-0 translate-middle badge rounded-pill bg-black">
+            <span className="position-absolute top-0 end-0 translate-middle badge rounded-pill bg-black">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0M14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1M8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
                 </svg>
